58481e684e4e158ef87d73c66463ad5ce8e57862
unknown <alighafoorzade@gmail.com>
2020-03-21 22:24:30 +0430

    Updates

---
+++ precache-manifest.47c2d1d16601540121d2e7947386c212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ea103922614610062f961eb8db8242",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "73e25f3b3c949e5ddb24",
+    "url": "/todo-react/static/css/main.ae80000b.chunk.css"
+  },
+  {
+    "revision": "3c5e153110000af768b0",
+    "url": "/todo-react/static/js/2.79be69aa.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todo-react/static/js/2.79be69aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e25f3b3c949e5ddb24",
+    "url": "/todo-react/static/js/main.df506903.chunk.js"
+  },
+  {
+    "revision": "cad832119259e5b2b141",
+    "url": "/todo-react/static/js/runtime-main.1c174b56.js"
+  }
+]);
